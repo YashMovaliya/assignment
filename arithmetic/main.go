da08693fc4d818ce9d86cadde1680d5fe4d1d44b
yashmovaliya1607@gmail.com <yashmovaliya1607@gmail.com>
2020-11-24 15:07:26 +0530

    arithmetic operations added

---
+++ arithmetic/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/add/{a}/{b}", Add).Methods("GET")
+	router.HandleFunc("/sub/{a}/{b}", Sub).Methods("GET")
+	router.HandleFunc("/mul/{a}/{b}", Mul).Methods("GET")
+	router.HandleFunc("/div/{a}/{b}", Div).Methods("GET")
+	router.HandleFunc("/mod/{a}/{b}", Mod).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+func Add(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	a := params["a"]
+	int1, err := strconv.ParseInt(a, 6, 12)
+	if err != nil {
+		fmt.Println(err)
+	}
+	b := params["b"]
+
+	int2, err := strconv.ParseInt(b, 6, 12)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var c int64 = int1 + int2
+	fmt.Printf("Answer is %d \n", c)
+}
+
+func Sub(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	a := params["a"]
+	int1, err := strconv.ParseInt(a, 6, 12)
+	if err != nil {
+		fmt.Println(err)
+	}
+	b := params["b"]
+
+	int2, err := strconv.ParseInt(b, 6, 12)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var c int64 = int1 - int2
+
+	fmt.Printf("Answer is %d \n", c)
+}
+func Mul(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	a := params["a"]
+	int1, err := strconv.ParseInt(a, 6, 12)
+	if err != nil {
+		fmt.Println(err)
+	}
+	b := params["b"]
+
+	int2, err := strconv.ParseInt(b, 6, 12)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var c int64 = int1 * int2
+
+	fmt.Printf("Answer is %d \n", c)
+}
+
+func Div(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	a := params["a"]
+	int1, err := strconv.ParseInt(a, 6, 12)
+	if err != nil {
+		fmt.Println(err)
+	}
+	b := params["b"]
+
+	int2, err := strconv.ParseInt(b, 6, 12)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var c int64 = int1 / int2
+
+	fmt.Printf("Answer is %d \n", c)
+}
+func Mod(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	a := params["a"]
+	int1, err := strconv.ParseInt(a, 6, 12)
+	if err != nil {
+		fmt.Println(err)
+	}
+	b := params["b"]
+
+	int2, err := strconv.ParseInt(b, 6, 12)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var c int64 = int1 % int2
+
+	fmt.Printf("Answer is %d \n", c)
+}
